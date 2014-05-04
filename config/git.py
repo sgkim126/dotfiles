b38fb004f4a0a52d33e0a4e3f3e4690b65c89bbf
Seulgi Kim <sgkim126@gmail.com>
2014-05-05 00:21:05 +0900

    Add config class to initialize git.

---
+++ config/git.py
@@ -0,0 +1,29 @@
+import config
+import os
+import shutil
+import subprocess
+
+
+class ConfigGit(config.Config):
+    def targets(self):
+        return [
+            'gitconfig',
+            'gitignore',
+        ]
+    def source_dir(self):
+        return os.path.join(super().source_dir(), 'home')
+    def post(self):
+        if subprocess.call(['git' ,'config', '--global', 'user.name']):
+            while True:
+                name = input("Enter your name for git: ")
+                if name != '':
+                    break
+            subprocess.call(['git', 'config', '--global', 'user.name', name])
+        if subprocess.call(['git' ,'config', '--global', 'user.email']):
+            while True:
+                mail = input("Enter your mail address for git: ")
+                if mail != '':
+                    break
+            subprocess.call(['git', 'config', '--global', 'user.email', mail])
+    def do(self, target):
+        shutil.copy(self.source_path(target), self.target_path(target))
