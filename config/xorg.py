a969c6c9860ea5a4b4881f1f225585c319f31f0c
Seulgi Kim <sgkim126@gmail.com>
2014-05-04 22:14:06 +0900

    Add config class to setting Xorg.

---
+++ config/xorg.py
@@ -0,0 +1,21 @@
+import config
+import os
+import subprocess
+
+
+class ConfigXorg(config.Config):
+    def targets(self):
+        return [
+            'evoluent.conf',
+            'kensington-slimblade.conf',
+        ]
+    def source_dir(self):
+        return os.path.join(super().source_dir(), 'xorg')
+    def target_dir(self):
+        return '/usr/share/X11/xorg.conf.d/'
+    def target_path(self, target):
+        return os.path.join(self.target_dir(), '80-' + target)
+    def backup_path(self, target):
+        return os.path.join(self.target_dir(), '90-' + target + '.backup')
+    def do(self, target):
+        subprocess.call(['sudo', 'ln', '-s', self.source_path(target), self.target_path(target)])
