133bcd2292752d9b9968d2bbf9924414399d258a
Seulgi Kim <sgkim126@gmail.com>
2014-05-04 22:11:25 +0900

    Add config to setting .root/bin directory.

---
+++ config/bin.py
@@ -0,0 +1,16 @@
+import config
+import os
+
+
+class ConfigBin(config.Config):
+    def targets(self):
+        return [
+            'git-new-workdir',
+        ]
+    def source_dir(self):
+        return os.path.join(super().source_dir(), 'bin')
+    def target_dir(self):
+        return os.path.join(os.path.join(super().target_dir(), '.root'), 'bin')
+    def target_path(self, target):
+        return os.path.join(self.target_dir(), target)
+
