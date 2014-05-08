4401c3de807c878bac28715108f8e3d1b1ade517
Seulgi Kim <sgkim126@gmail.com>
2014-05-08 10:40:18 +0900

    Install jdk

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
                 'ctags',
                 'git',
                 'irssi',
+                'openjdk-7-jdk',
                 'python2.7',
                 'python3',
                 'ri',
