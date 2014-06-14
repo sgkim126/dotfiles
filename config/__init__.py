e0c2222787e565c9221713fe7883523c02fa7e8d
Seulgi Kim <sgkim126@gmail.com>
2014-06-15 00:59:37 +0900

    No needs to use abc module.
    Config is not an abstract class.

--- config/__init__.py
+++ config/__init__.py
@@ -1,38 +1,28 @@
-from abc import ABCMeta
-from abc import abstractmethod
 import os
 import shutil
 import sys
 
 
 class Config:
-    __metaclass__ = ABCMeta
-
     def __init__(self, *targets):
         self.targets = targets
         pass
 
-    @abstractmethod
     def source_dir(self):
         return os.path.abspath(os.path.curdir)
 
-    @abstractmethod
     def destination_dir(self):
         return os.getenv('HOME')
 
-    @abstractmethod
     def source_path(self, target):
         return os.path.join(self.source_dir(), target)
 
-    @abstractmethod
     def destination_path(self, target):
         return os.path.join(self.destination_dir(), '.' + target)
 
-    @abstractmethod
     def backup_path(self, target):
         return os.path.join(self.destination_dir(), target + '.backup')
 
-    @abstractmethod
     def source_exists(self, target):
         if not os.path.exists(self.source_path(target)):
             print(
@@ -41,7 +31,6 @@ class Config:
             return False
         return True
 
-    @abstractmethod
     def resolve_conflict(self, target):
         if os.path.exists(self.destination_path(target)):
             destination_path = self.destination_path(target)
@@ -53,15 +42,12 @@ class Config:
             shutil.move(destination_path, backup_path)
         return True
 
-    @abstractmethod
     def do(self, target):
         os.symlink(self.source_path(target), self.destination_path(target))
 
-    @abstractmethod
     def pre(self):
         pass
 
-    @abstractmethod
     def post(self):
         pass
 
