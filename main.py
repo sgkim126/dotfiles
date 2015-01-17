c4251f5c493d30d153bcd9da4912acc95789e277
Seulgi Kim <dev@seulgi.kim>
2015-01-17 21:21:10 +0900

    Remove libraries.
    I'll use brew.

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-import config.ant
 import config.apt
 import config.bin
 import config.brew
 import config.git
 import config.home
-import config.node
 import config.root
-import config.sbt
-import config.scala
 import config.vim
-import config.virtualenv
 import config.xorg
 
 
@@ -27,25 +22,22 @@ if __name__ == '__main__':
     try:
         if confirm('Do you want to install packages with sudo?(y/n) '):
             config.apt.ConfigApt(
-                'ack-grep',
                 'build-essential',
                 'clang',
-                'ctags',
-                'curl',
-                'git',
-                'libpython2.7-dev',
-                'openjdk-7-jdk',
-                'openvpn',
-                'python2.7',
-                'python3',
-                'ri',
-                'ruby',
-                'screen',
-                'tmux',
-                'vim'
+                'libpython2.7-dev'
             ).run()
     except Exception as ex:
         print(ex)
+
+    try:
+        if confirm('Do you want to config Xorg with sudo?(y/n) '):
+            config.xorg.ConfigXorg(
+                'evoluent.conf',
+                'kensington-slimblade.conf'
+            ).run()
+    except Exception as ex:
+        print(ex)
+
     try:
         if confirm('Do you want to create $HOME/.root directory?(y/n) '):
             config.root.ConfigRoot(
@@ -57,11 +49,13 @@ if __name__ == '__main__':
             ).run()
     except Exception as ex:
         print(ex)
+
     try:
         if confirm('Do you want to config bin?(y/n) '):
             config.bin.ConfigBin('git-new-workdir').run()
     except Exception as ex:
         print(ex)
+
     try:
         if confirm('Do you want to config home?(y/n) '):
             config.home.ConfigHome(
@@ -75,6 +69,7 @@ if __name__ == '__main__':
             ).run()
     except Exception as ex:
         print(ex)
+
     try:
         if confirm('Do you want to config git?(y/n) '):
             config.git.ConfigGit(
@@ -83,6 +78,7 @@ if __name__ == '__main__':
             ).run()
     except Exception as ex:
         print(ex)
+
     try:
         if confirm('Do you want to config vim?(y/n) '):
             config.vim.ConfigVim('vimrc').run()
@@ -107,42 +103,5 @@ if __name__ == '__main__':
                     'zlib1g-dev'
                 ).run()
             config.brew.ConfigBrew().run()
-    except Exception as ex:
-        print(ex)
-
-    try:
-        if confirm('Do you want to config Xorg with sudo?(y/n) '):
-            config.xorg.ConfigXorg(
-                'evoluent.conf',
-                'kensington-slimblade.conf'
-            ).run()
-    except Exception as ex:
-        print(ex)
-    try:
-        if confirm('Do you want to install virtualenv?(y/n) '):
-            config.virtualenv.ConfigVirtualenv('1.11.6').run()
-    except Exception as ex:
-        print(ex)
-    try:
-        if confirm('Do you want to install node?(y/n) '):
-            config.node.ConfigNode('v0.11.13').run()
-    except Exception as ex:
-        print(ex)
-
-    try:
-        if confirm('Do you want to install ant?(y/n) '):
-            config.ant.ConfigAnt('ANT_194').run()
-    except Exception as ex:
-        print(ex)
-
-    try:
-        if confirm('Do you want to install scala?(y/n) '):
-            config.scala.ConfigScala('v2.11.2').run()
-    except Exception as ex:
-        print(ex)
-
-    try:
-        if confirm('Do you want to install sbt?(y/n) '):
-            config.sbt.ConfigSbt('v0.13.5').run()
     except Exception as ex:
         print(ex)
