2f3ab94a110b6688a453227773e7a39f8ad05327
Seulgi Kim <dev@seulgi.kim>
2026-04-03 14:24:10 +0900

    [nvim] setup lazy.nvim

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -1,6 +1,28 @@
 -- Source ~/.vimrc
 vim.cmd("source " .. vim.fn.expand("~/.vimrc"))
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- Load non-plugin settings
 require("config.options")
 require("config.keymaps")
+
+-- Setup lazy.nvim (Looks for plugins in lua/plugins/)
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+  checker = { enabled = true },
+})
